1e7e850fa99397ecbd41c369407dc90cf30941bb
Challenger <87441261+challenger-hkn@users.noreply.github.com>
2022-05-26 20:58:55 +0300

    Update app.py

--- Employees_Churn_up_HR/app.py
+++ Employees_Churn_up_HR/app.py
@@ -33,6 +33,7 @@ st.sidebar.markdown(
 			"<h1 style='text-align:center;\
 						color: red;font-family:tahoma;font-size:115%;'> Human is the most valuable asset.\
 						Retain your employees.\
+						</h1>", unsafe_allow_html=True)
 
 im = Image.open("churn.png")
 st.image(im, width=300)
